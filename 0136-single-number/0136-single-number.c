a014d0cf429a49bdca1f4f01b05df7d2393a7d07
karanm6505 <83998537+karanm6505@users.noreply.github.com>
2025-07-27 18:06:04 +0530

    Time: 0 ms (100%), Space: 9.4 MB (31.84%) - LeetHub

--- 0136-single-number/0136-single-number.c
+++ 0136-single-number/0136-single-number.c
@@ -1,15 +1,9 @@
-int sort(const void *a, const void *b)
-{
-    return (*(int *)a - *(int*)b);
-}
+
 int singleNumber(int* nums, int numsSize) 
 {
-    qsort(nums, numsSize, sizeof(int), sort);
-    for (int i = 0; i < numsSize - 1; i += 2) 
-    {
-        if (nums[i] != nums[i + 1]) {
-            return nums[i];
-        }
+    int result = 0;
+    for (int i = 0; i < numsSize; i++) {
+        result ^= nums[i];
     }
-    return nums[numsSize - 1];
+    return result;
 }
