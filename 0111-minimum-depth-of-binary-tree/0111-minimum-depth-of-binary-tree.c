52bad0e4e6f9a7b549db8beb2b090ebcdda8f6ec
karanm6505 <83998537+karanm6505@users.noreply.github.com>
2025-08-04 11:01:28 +0530

    Time: 15 ms (54.67%), Space: 80.7 MB (13.27%) - LeetHub

---
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.c
@@ -0,0 +1,20 @@
+int min(int a, int b)
+{
+    return a < b ? a : b;
+}
+int minDepth(struct TreeNode* root) 
+{
+    if (root == NULL)
+        return 0;
+
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+
+    if (root->left == NULL)
+        return 1 + minDepth(root->right);
+
+    if (root->right == NULL)
+        return 1 + minDepth(root->left);
+
+    return 1 + min(minDepth(root->left), minDepth(root->right));
+}
